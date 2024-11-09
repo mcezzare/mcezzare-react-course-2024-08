7f0e064902b80d07a0ab56db3ad9f8abf28b811e
Mario Cezzare <mario.chiodi@catho.com>
2024-11-08 21:21:21 -0300

    add react native paper and PaperProvider

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -1,15 +1,18 @@
+/* eslint-disable react/react-in-jsx-scope */
 import 'react-native-gesture-handler';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './presentation/navigator/StackNavigator';
+import { PaperProvider } from 'react-native-paper';
 
 // export const PokedexApp = ( { children }: PropsWithChildren ) => {
 export const PokedexApp = () => {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-      {/* { children } */ }
-    </NavigationContainer>
-
+    <PaperProvider>
+      <NavigationContainer>
+        <StackNavigator />
+        {/* { children } */ }
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,8 +1,12 @@
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-paper';
 export const HomeScreen = () => {
   return (
     <View>
       <Text style={ { fontSize: 30 } }>HomeScreen</Text>
+      <Button mode="contained" onPress={ () => console.log( 'Pressed' ) }>
+        Press me
+      </Button>
     </View>
   );
 };

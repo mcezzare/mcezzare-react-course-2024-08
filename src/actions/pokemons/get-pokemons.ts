526493623082c15ed210441fe41d7b06f7f58b15
Mario Cezzare <mario.chiodi@catho.com>
2024-11-10 22:29:41 -0300

    end 232 background color card using async method and

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -1,34 +1,32 @@
-import {pokeApi} from '../../config/api/pokeApi';
-import type {Pokemon} from '../../domain/entities/pokemon';
+import { pokeApi } from '../../config/api/pokeApi';
+import type { Pokemon } from '../../domain/entities/pokemon';
 import type {
   PokeAPIPaginatedResponse,
   PokeAPIPokemon,
 } from '../../infrastructure/interfaces/pokepi.interfaces';
-import {PokemonMapper} from '../../infrastructure/mappers/pokemon.mapper';
+import { PokemonMapper } from '../../infrastructure/mappers/pokemon.mapper';
 
 export const getPokemons = async (
   page: number,
   limit: number = 20,
 ): Promise<Pokemon[]> => {
   try {
-    const url = `/pokemon?offset=${page * 10}&limit=${limit}`;
-    const {data} = await pokeApi.get<PokeAPIPaginatedResponse>(url);
+    const url = `/pokemon?offset=${ page * 10 }&limit=${ limit }`;
+    const { data } = await pokeApi.get<PokeAPIPaginatedResponse>( url );
 
-    const pokemonPromises = data.results.map(info => {
-      return pokeApi.get<PokeAPIPokemon>(info.url);
-    });
+    const pokemonPromises = data.results.map( info => {
+      return pokeApi.get<PokeAPIPokemon>( info.url );
+    } );
 
-    const pokeApiPokemons = await Promise.all(pokemonPromises);
-    const pokemons = pokeApiPokemons.map(item =>
-      PokemonMapper.pokeApiPokemonToEntity(item.data),
+    const pokeApiPokemons = await Promise.all( pokemonPromises );
+    const pokemonsPromises = pokeApiPokemons.map( item =>
+      PokemonMapper.pokeApiPokemonToEntity( item.data ),
     );
 
-    console.log(pokemons[0]);
+    return await Promise.all( pokemonsPromises );
 
-    return pokemons;
-
-  } catch (error) {
-    console.log(error);
-    throw new Error('Error getting pokemons');
+  } catch ( error ) {
+    console.log( error );
+    throw new Error( 'Error getting pokemons' );
   }
 };
--- src/infrastructure/mappers/pokemon.mapper.ts
+++ src/infrastructure/mappers/pokemon.mapper.ts
@@ -1,3 +1,4 @@
+import { getColorFromImage } from '../../config/helpers/get-color';
 import type { Pokemon } from '../../domain/entities/pokemon';
 import type { PokeAPIPokemon } from '../interfaces/pokepi.interfaces';
 
@@ -6,25 +7,27 @@ import type { PokeAPIPokemon } from '../interfaces/pokepi.interfaces';
 export class PokemonMapper {
 
 
-  static pokeApiPokemonToEntity( data: PokeAPIPokemon ): Pokemon {
+  static async pokeApiPokemonToEntity( data: PokeAPIPokemon ): Promise<Pokemon> {
 
-    const sprites = PokemonMapper.getSprites(data);
-    const avatar = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`;
+    const sprites = PokemonMapper.getSprites( data );
+    const avatar = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${ data.id }.png`;
 
 
+    const color = await getColorFromImage( avatar );
 
     return {
-      id: data.id ,
+      id: data.id,
       name: data.name,
       avatar: avatar,
       sprites: sprites,
-      types: data.types.map( type => type.type.name )
-    }
+      types: data.types.map( type => type.type.name ),
+      color: color,
+    };
 
   }
 
 
-  static getSprites(data: PokeAPIPokemon): string[] {
+  static getSprites( data: PokeAPIPokemon ): string[] {
     const sprites: string[] = [
       data.sprites.front_default,
       data.sprites.back_default,
@@ -32,16 +35,16 @@ export class PokemonMapper {
       data.sprites.back_shiny,
     ];
 
-    if (data.sprites.other?.home.front_default)
-      sprites.push(data.sprites.other?.home.front_default);
-    if (data.sprites.other?.['official-artwork'].front_default)
-      sprites.push(data.sprites.other?.['official-artwork'].front_default);
-    if (data.sprites.other?.['official-artwork'].front_shiny)
-      sprites.push(data.sprites.other?.['official-artwork'].front_shiny);
-    if (data.sprites.other?.showdown.front_default)
-      sprites.push(data.sprites.other?.showdown.front_default);
-    if (data.sprites.other?.showdown.back_default)
-      sprites.push(data.sprites.other?.showdown.back_default);
+    if ( data.sprites.other?.home.front_default )
+      sprites.push( data.sprites.other?.home.front_default );
+    if ( data.sprites.other?.[ 'official-artwork' ].front_default )
+      sprites.push( data.sprites.other?.[ 'official-artwork' ].front_default );
+    if ( data.sprites.other?.[ 'official-artwork' ].front_shiny )
+      sprites.push( data.sprites.other?.[ 'official-artwork' ].front_shiny );
+    if ( data.sprites.other?.showdown.front_default )
+      sprites.push( data.sprites.other?.showdown.front_default );
+    if ( data.sprites.other?.showdown.back_default )
+      sprites.push( data.sprites.other?.showdown.back_default );
 
     return sprites;
   }

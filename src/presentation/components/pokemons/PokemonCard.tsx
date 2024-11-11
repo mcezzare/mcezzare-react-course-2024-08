6ba35721e057ae6e220e8c8ccf1298dbf5ee7837
Mario Cezzare <mario.chiodi@catho.com>
2024-11-10 21:45:27 -0300

    add animations files

--- src/presentation/components/pokemons/PokemonCard.tsx
+++ src/presentation/components/pokemons/PokemonCard.tsx
@@ -3,6 +3,7 @@
 import { Image, StyleSheet, View } from 'react-native';
 import { Pokemon } from '../../../domain/entities/pokemon';
 import { Card, Text } from 'react-native-paper';
+import { FadeInImage } from '../ui/FadeInImage';
 
 interface Props {
   pokemon: Pokemon;
@@ -27,8 +28,8 @@ export const PokemonCard = ( { pokemon }: Props ) => {
       </View>
 
       {/* POkemon Image */ }
-      <Image
-        source={ { uri: pokemon.avatar } }
+      <FadeInImage
+        uri={ pokemon.avatar }
         style={ styles.pokemonImage }
       />
 
